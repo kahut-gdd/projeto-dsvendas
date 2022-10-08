d56f7eba99cfe3f2d7c0b11d83bca1c85682ccb8
kahut.dev <dagmar.kahutpp@gmail.com>
2022-10-08 14:20:08 -0300

    Securty config

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -2,7 +2,7 @@ import kahutDev from 'assets/img/kahutDev.png'
 
 function NavBar() {
     return (
-        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-dark border-bottom shadow-sm">
         <div className="container">
           <nav className="my-2 my-md-0 mr-md-3">
             <img src={kahutDev} alt="kahutDev" width="120" />
