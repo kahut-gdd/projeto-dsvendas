cf9ae1876669311603960f24e8e2de60411af622
kahut.dev <dagmar.kahutpp@gmail.com>
2022-10-12 10:21:06 -0300

    Firts request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,27 +1,51 @@
+import axios from 'axios';
 import Chart from  'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/requests';
+
+type ChartData = {
+  labels: string[];
+  series: number[];
+}
 
 const DonutChart = () => {
-    
-    const mockData = {
-      
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Lucas Gabriel', 'Maria', 'Esther', 'Daniele', 'Renato']
-        }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let chartData : ChartData = { labels: [], series: []};
+
+axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    .then(response => {
+      const data = response.data as SaleSum[];
+      const myLabels = data.map(x => x.sellerName);
+      const mySeries = data.map(x => x.sum);
+      
+      chartData = { labels: myLabels, series: mySeries};
+      console.log(chartData);
+    });
+
+    
+    //const mockData = {      
+      //  series: [477138, 499928, 444867, 220426, 473088],
+      // labels: ['Lucas Gabriel', 'Maria', 'Esther', 'Daniele', 'Renato']
+      // }
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const options = {
             legend: {
                 show: true
             }
         
     }
-  return (
-    <Chart 
-        options = {{ ...options, labels: mockData.labels}}
-        series={mockData.series}
+
+    return (
+      <Chart
+        options={{ ...options, labels: chartData.labels}}
+        series={chartData.series}
         type="donut"
-        heigth= "240"
-    />
-  );
+        height="240"
+       /> 
+    );
+
 }
 
 export default DonutChart;
--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,11 +1,14 @@
 import kahutDev from 'assets/img/kahutDev.png'
+import { Link } from 'react-router-dom';
 
 function NavBar() {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-dark border-bottom shadow-sm">
         <div className="container">
           <nav className="my-2 my-md-0 mr-md-3">
-            <img src={kahutDev} alt="kahutDev" width="120" />
+            <Link to="/">
+              <img src={kahutDev} alt="kahutDev" width="120" />
+            </Link>
           </nav>
         </div>
       </div>
