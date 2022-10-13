3924240508f740dfabb944dc27b5a2c78b3628a2
kahut.dev <dagmar.kahutpp@gmail.com>
2022-10-12 23:21:08 -0300

    DataTable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,56 +1,53 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "utils/format";
+import { BASE_URL } from "utils/requests";
+
 
 const DataTable = () => {
-  return (
-    <div className="table-responsive">
-        <table className="table table-striped table-sm">
-            <thead>
-                <tr>
-                    <th>Data</th>
-                    <th>Vendedor</th>
-                    <th>Clientes visitados</th>
-                    <th>Negócios fechados</th>
-                    <th>Valor</th>
+
+    const [page, setPage] = useState<SalePage>({
+        first: true,
+        last: true,
+        number: 0,
+        totalElements: 0,
+        totalPages: 0
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
+            .then(response => {
+                setPage(response.data);
+            });
+    }, []);
+
+    return (
+      <div className="table-responsive">
+          <table className="table table-striped table-sm">
+              <thead>
+                  <tr>
+                      <th>Data</th>
+                      <th>Vendedor</th>
+                      <th>Clientes visitados</th>
+                      <th>Negócios fechados</th>
+                      <th>Valor($)</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  {page.content?.map(item => (
+                    <tr key={item.id}>
+                    <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
+                    <td>{item.seller.name}</td>
+                    <td>{item.visited}</td>
+                    <td>{item.deals}</td>
+                    <td>{item.amount.toFixed(2)}</td>
                 </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>22/04/2022</td>
-                    <td>Lucas Gabriel</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
-                </tr>
-            </tbody>
-            <tbody>
-                <tr>
-                    <td>22/04/2022</td>
-                    <td>Maria</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
-                </tr>
-            </tbody>
-            <tbody>
-                <tr>
-                    <td>22/04/2022</td>
-                    <td>Daniele</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
-                </tr>
-            </tbody>
-            <tbody>
-                <tr>
-                    <td>22/04/2022</td>
-                    <td>Esther</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-  );
+                  ))}                                
+              </tbody>
+          </table>
+      </div>
+    );
 }
 
 export default DataTable;
